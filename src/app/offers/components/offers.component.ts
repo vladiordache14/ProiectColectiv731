ab689e38a0fb25ad72d3c187659b0f149c9875df
galtimea2002 <timea.gal1@stud.ubbcluj.ro>
2023-11-18 21:21:39 +0200

    list of offers from backend

--- src/app/offers/components/offers.component.ts
+++ src/app/offers/components/offers.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Offer} from "../offer";
+import {OffersService} from "../service/offers.service";
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.component.html',
   styleUrls: ['./offers.component.css']
 })
-export class OffersComponent {
+export class OffersComponent implements OnInit{
+  offers: Offer[] = [];
 
+  constructor(private offersService: OffersService) { }
+
+  ngOnInit(): void {
+    this.offersService.getAllOffers().subscribe(offers => {
+      this.offers = offers;
+    });
+  }
 }
--- src/app/offers/service/offers.service.ts
+++ src/app/offers/service/offers.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {catchError, map, Observable} from "rxjs";
+import {Offer} from "../offer";
 
 @Injectable({
   providedIn: 'root'
 })
 export class OffersService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  // not working yet:
+  getAllOffers(): Observable<Offer[]> {
+    return this.http.get<Offer[]>("http://localhost:8080/offers/all").pipe(
+      map(offers => offers.sort((a, b) => a.name.localeCompare(b.name))),
+      catchError(error => {
+        console.error('Error fetching offers:', error);
+        throw error;
+      })
+    );
+  }
 }
