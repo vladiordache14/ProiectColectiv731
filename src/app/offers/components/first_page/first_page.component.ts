dc43acad2e3e0a1ae9dfba65de1f2daff3c320af
atletul <46723813+vladiordache14@users.noreply.github.com>
2023-12-20 09:59:58 +0200

    new developer fixes

--- src/app/offers/components/first_page/first_page.component.ts
+++ src/app/offers/components/first_page/first_page.component.ts
@@ -129,6 +129,7 @@ export class First_pageComponent {
             this.addressInvalid = false;
             this.phone = "";
             this.phoneInvalid = false;
+            this.router.navigate(['/page-one'])
             console.log(response);
           },
           error: (error: any) => {
