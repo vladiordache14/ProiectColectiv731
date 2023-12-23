ce3e31c81de571a1872becc13639ff97fbfa3470
atletul <46723813+vladiordache14@users.noreply.github.com>
2023-12-23 18:32:36 +0200

    Revert "Autentificare"

--- src/app/offers/adverts-routing.module.ts
+++ src/app/offers/adverts-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {AdvertsComponent} from "./components/adverts.component";
 import {RouterModule, Routes} from "@angular/router";
 import { PageTwoComponent } from './components/page-two/page-two.component';
-import {AdvertsComponent} from "./components/adverts.component";
-
 
 
 const routes: Routes = [
   { path: 'page-one', component: AdvertsComponent },
   { path: 'page-two', component: PageTwoComponent },
+  { path: '', redirectTo: '/page-one', pathMatch: 'full' },
 ];
-
 
 @NgModule({
   imports: [
